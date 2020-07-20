4ef87f92479d57c2f6b6e9867915513869598434
Kamil <qkiz288@gmail.com>
2020-07-20 10:56:00 +0200

    Created BeerService and it's implementation

---
+++ src/main/java/guru/sfg/beer/order/service/services/beer/model/BeerDto.java
@@ -0,0 +1,25 @@
+package guru.sfg.beer.order.service.services.beer.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+public class BeerDto {
+
+    private UUID id;
+    private Integer version;
+
+    @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ssZ", shape=JsonFormat.Shape.STRING)
+    private OffsetDateTime createdDate;
+
+    @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ssZ", shape=JsonFormat.Shape.STRING)
+    private OffsetDateTime lastModifiedDate;
+    private String beerName;
+    private BeerStyleEnum beerStyle;
+    private String upc;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
+    private BigDecimal price;
+    private Integer quantityOnHand;
+}
